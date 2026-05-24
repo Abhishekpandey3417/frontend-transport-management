afc3a804e02aaec11be777ff8dc54a1cae9fbbe5
Abhishekpandey3417 <abhishekpandey06102002@gmail.com>
2026-05-24 18:29:17 +0530

    fix.env

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5001", {
+const socket = io("https://backend-transport-management-production.up.railway.app", {
     withCredentials: true,
     transports: ["websocket", "polling"],
     autoConnect: true,
